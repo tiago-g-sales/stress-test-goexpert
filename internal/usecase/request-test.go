6f68a0e3a17b7815437ab7320abedf30b9cf525b
Tiago Sales <tiagosales@outlook.com>
2025-02-05 21:11:58 -0300

    Update project implements logic concurrecy and total request

--- internal/usecase/request-test.go
+++ internal/usecase/request-test.go
@@ -8,48 +8,54 @@ import (
 	"github.com/tiago-g-sales/stress-test-goexpert/internal/model"
 )
 
-
-
-
-func Execute(model model.ParameterRequestDTO) {
-	var wg sync.WaitGroup
-
-
-	fmt.Printf("URL: %s \n" , model.Url)
-	fmt.Printf("Requests: %d \n", model.Requests)	
-	fmt.Printf("Concurrency: %d \n", model.Concurrency)	
-
-
-	concorrencia := make(chan int, model.Concurrency)
-
-
-
-	for i := int64(0); i < model.Requests; i++ {
-		wg.Add(1)
-		concorrencia <- 1	
-		go func (){
-
-				defer wg.Done()
-				defer func() { <-concorrencia }()
-				
-				req, err := http.NewRequest("GET", model.Url, nil)
-				if err != nil {
-					panic(err)
-				}
-				res, err := http.DefaultClient.Do(req)
-				if err != nil {
-					panic(err)
-				}
-				if res.StatusCode == 200 {
-					print("ok")
-				} else {
-					print("nok")
-				}
-			
-				defer res.Body.Close()
-
-			}()
-		}
-
+func Execute(m model.ParameterRequestDTO) {
+	report := model.ReportTotalInfo{}
+	ch := make(chan int)
+	wg := sync.WaitGroup{}
+	wg.Add(int(m.Requests))	
+	
+	go request(m, ch )
+	for i := int64(1); i <= m.Concurrency; i++ {
+	 	go	ExecuteConcurrency(&i,  m.Url, &report, &wg,ch)
+	}
 	wg.Wait()
+	fmt.Printf("Total Requests OK: %d \n", report.StatusOK)	
+	fmt.Printf("Total Requests NOK: %d \n", report.StatusNOK)
+}
+
+
+
+
+func ExecuteConcurrency(i *int64, m string, report  *model.ReportTotalInfo, wg *sync.WaitGroup,  ch chan int)  {
+	fmt.Printf("Executing goroutine %d \n", uint64(*i))
+
+	for i := range ch {
+		req, err := http.NewRequest("GET", m, nil)
+		if err != nil {
+			panic(err)
+		}
+		res, err := http.DefaultClient.Do(req)
+		if err != nil {
+			panic(err)
+		}
+		if res.StatusCode == 200 {
+			report.StatusOK++	
+		} else {
+			report.StatusNOK++
+		}
+		
+		if i != 0 {}	
+		wg.Done()
+	}
+
+}
+
+
+func request(m model.ParameterRequestDTO,  ch chan int) {
+	for i := 0; i <= int(m.Requests); i++ {
+		ch <- i
+	}
+	close(ch)
+	
+	
 }
